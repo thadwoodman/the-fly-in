e9fa9974ae6c6905f5625c7cbb775c864371dd92
0kcupid <annaploegh@gmail.com>
2018-05-13 23:56:06 -0700

    floating animation

--- scene.tsx
+++ scene.tsx
@@ -1,53 +1,48 @@
 
 import { createElement, ScriptableScene } from "metaverse-api";
+const networkHz = 10
+const interval = 1000 / networkHz
+export default class FlyIn extends ScriptableScene<any, { time: number}> {
+   	state = { time: 0}
+   	timeout = setInterval(()=>{
+   		this.setState({
+   			time: performance.now() * 0.001
+   		})
+   	}, interval)
+   	sceneWillUnmount(){
+   		clearInterval(this.timeout)
+   	}
 
-export default class SharkAnimation extends ScriptableScene {
-    state = {
-        bitestate: false,
-        dancestate: false
-    };
 
-    async sceneDidMount() {
-        this.eventSubscriber.on(`supershark_click`, () => this.clickedOnShark());
-    }
-
-    clickedOnShark() {
-        this.setState({ bitestate: !this.state.bitestate });
-    }
-
-    async render() {
-        return (
+   	async render() {
+   		const float = Math.cos(this.state.time)
+   		const glide = Math.sin(this.state.time)
+         return (
             <scene>
 
 	                <gltf-model
 	                    id="greenship"
-	                    position={{ x: 3, y: .03, z: 5 }}
+	                    position={{ x: (3 + glide/6), y: (.03 + float/10), z: 5 }}
 	                    scale={.5}
 	                    src="models/sgreen.glb"
 	                />
 	                 <gltf-model
 	                    id="pinkship"
-	                    position={{ x: 5, y: .05, z: 8 }}
+	                    position={{ x: (5 + float/8), y: (.05 + float/4), z: 8 }}
 	                    scale={.5}
 	                    src="models/spink.glb"
 	                />
 	                 <gltf-model
 	                    id="blueship"
-	                    position={{ x: 3, y: .05, z: 6 }}
+	                    position={{ x: (3 + float/10), y: (.05 + glide/12), z: 6 }}
 	                    scale={.5}
 	                    src="models/sblue.glb"
 	                />
 	                 <gltf-model
 	                    id="orangeship"
-	                    position={{ x: 7, y: .02, z: 2 }}
+	                    position={{ x: (7 + float/12), y: (.02 + glide/4), z: 2 }}
 	                    scale={.5}
 	                    src="models/sorang.glb"
-	                />
-	                <gltf-model
-	                    id="screen"
-	                    position={{ x: 7, y: .03, z: 3 }}
-	                    scale={0.3}
-	                  //  src="models/screentest2.glb"
 	                />
 	                <plane
 	                    id="movie"
@@ -55,7 +50,7 @@ export default class SharkAnimation extends ScriptableScene {
 	                    scale={2.5}
 	                    height={1}
 	                    width={2}
-	                    material={{src: "video/divine.gif"}}
+	                    material={{src: "image/fmt.jpg"}}
 	                    rotation={{x: 0, y: 0, z: 0}}
 
 	                />
