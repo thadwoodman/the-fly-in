27ecef77ee9b3a757c6389adfdd902805a125ac0
0kcupid <annaploegh@gmail.com>
2018-05-13 21:42:55 -0700

    new land and spaceships

--- scene.tsx
+++ scene.tsx
@@ -18,48 +18,53 @@ export default class SharkAnimation extends ScriptableScene {
     async render() {
         return (
             <scene>
-                <gltf-model
-                    id="redcar"
-                    position={{ x: 2, y: 2, z: 6 }}
-                    scale={0.3}
-                    src="models/fcf.glb"
-                />
-                 <gltf-model
-                    id="bluecar"
-                    position={{ x: 5, y: 5, z: 3 }}
-                    scale={0.3}
-                    src="models/fcfblue.glb"
-                />
-                 <gltf-model
-                    id="graycar"
-                    position={{ x: 7, y: 7, z: 1 }}
-                    scale={0.3}
-                    src="models/fcfgray.glb"
-                />
-                <gltf-model
-                    id="screen"
-                    position={{ x: 7, y: 2, z: 6 }}
-                    scale={0.3}
-                  //  src="models/screentest2.glb"
-                />
-                <plane
-                    id="movie"
-                    position={{x: 9.5, y: 3, z: 5}}
-                    scale={2.5}
-                    height={1}
-                    width={2}
-                    material={{src: "video/divine.gif"}}
-                    rotation={{x: 0, y: 90, z: 0}}
 
-                />
-                <plane
-                id="ground"
-                position={{x: 5, y: 0, z: 5}}
-                scale={10}
-                material={{src: "image/ground.jpg"}}
-                rotation={{x: 90, y: 0, z: 0}}
+	                <gltf-model
+	                    id="greenship"
+	                    position={{ x: 3, y: .03, z: 5 }}
+	                    scale={.5}
+	                    src="models/sgreen.glb"
+	                />
+	                 <gltf-model
+	                    id="pinkship"
+	                    position={{ x: 5, y: .05, z: 8 }}
+	                    scale={.5}
+	                    src="models/spink.glb"
+	                />
+	                 <gltf-model
+	                    id="blueship"
+	                    position={{ x: 3, y: .05, z: 6 }}
+	                    scale={.5}
+	                    src="models/sblue.glb"
+	                />
+	                 <gltf-model
+	                    id="orangeship"
+	                    position={{ x: 7, y: .02, z: 2 }}
+	                    scale={.5}
+	                    src="models/sorang.glb"
+	                />
+	                <gltf-model
+	                    id="screen"
+	                    position={{ x: 7, y: .03, z: 3 }}
+	                    scale={0.3}
+	                  //  src="models/screentest2.glb"
+	                />
+	                <plane
+	                    id="movie"
+	                    position={{x: 5, y: 3, z: 0}}
+	                    scale={2.5}
+	                    height={1}
+	                    width={2}
+	                    material={{src: "video/divine.gif"}}
+	                    rotation={{x: 0, y: 0, z: 0}}
 
-                />
+	                />
+	                <gltf-model
+	                id="ground"
+	                position={{x: 5, y: 0, z: 5}}
+	                src="models/land.glb"
+	                scale={.999}
+	                />
             </scene>
         );
     }
